7c2ed250087856bc785f515150d1cbca386fc7b2
Andrew Ma <maastergotmail@gmail.com>
2015-10-17 02:18:30 -0400

    why so hard

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+        //why
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
